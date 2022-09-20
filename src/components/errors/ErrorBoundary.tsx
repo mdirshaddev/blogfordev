749e80395672eb581fa64c44b3653594a545145e
Md Irshad <mdirshaddev+github@gmail.com>
2022-09-20 11:51:56 +0530

    chore(error): 🚨 Error boundary component
    later we will be capturing errors in sentry after sentry integration is done

---
+++ src/components/errors/ErrorBoundary.tsx
@@ -0,0 +1,82 @@
+import type { ErrorInfo, FC, ReactNode } from "react";
+import { Component, createContext, Fragment, useContext } from "react";
+
+export interface ErrorBoundaryState {
+  error: Error | null;
+}
+
+export interface ErrorFallbackProps {
+  error: Error;
+  onReset: () => void;
+}
+
+export const ErrorBoundaryContext = createContext<ErrorFallbackProps | null>(
+  null
+);
+
+export function useError(): ErrorFallbackProps | null {
+  const errorBoundaryContext = useContext(ErrorBoundaryContext);
+
+  if (errorBoundaryContext !== null) {
+    ("`useError` must be nested inside an `ErrorBoundaryProvider`.");
+  }
+
+  return errorBoundaryContext;
+}
+
+export interface ErrorBoundaryProps {
+  children?: ReactNode;
+  fallback: FC<ErrorFallbackProps> | JSX.Element;
+  onError?: (_error: Error, _info: ErrorInfo) => void;
+  onReset?: () => void;
+}
+
+const initialState = { error: null };
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+
+    this.onReset = this.onReset.bind(this);
+
+    this.state = initialState;
+  }
+
+  static getDerivedStateFromError(error: Error): Partial<ErrorBoundaryState> {
+    return { error };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    this.props.onError?.(error, info);
+  }
+
+  onReset(): void {
+    this.props.onReset?.();
+    this.setState(initialState);
+  }
+
+  render(): JSX.Element {
+    const { error } = this.state;
+
+    if (error !== null) {
+      const { fallback: Fallback } = this.props;
+
+      const contextValue = { error, onReset: this.onReset };
+
+      return (
+        <ErrorBoundaryContext.Provider value={contextValue}>
+          {typeof Fallback === "function" ? (
+            <Fallback {...contextValue} />
+          ) : (
+            Fallback
+          )}
+        </ErrorBoundaryContext.Provider>
+      );
+    }
+
+    return <Fragment>{this.props.children}</Fragment>;
+  }
+}
